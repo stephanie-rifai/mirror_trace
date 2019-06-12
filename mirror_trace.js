722fe8728d922c8939decec72093ac8c0103ebd8
stephanie-rifai <49794716+stephanie-rifai@users.noreply.github.com>
2019-06-12 13:03:15 -0400

    Update mirror_trace.js

--- mirror_trace.js
+++ mirror_trace.js
@@ -18,10 +18,10 @@
 var materials = {
 		'mirror' : [false, true, true, true, true, true, true, true],
 		'file_names' : ["https://raw.githubusercontent.com/stephanie-rifai/mirror_trace/master/sample.png", "https://raw.githubusercontent.com/stephanie-rifai/mirror_trace/master/1hard.png", "https://raw.githubusercontent.com/stephanie-rifai/mirror_trace/master/1easy.png"],
-		'xstarts' : [45,	27,		40],
-		'ystarts' : [411,	275,	45],
-		'xends' :   [575,	370,	368],
-		'yends' :    [31,	28,		267]
+		'xstarts' : [45,	38,		38],
+		'ystarts' : [411,	398,	401],
+		'xends' :   [575,	567,	562],
+		'yends' :    [31,	49,		54]
 	}
 	
 	//	'xstarts' : [47,	27,		40,		280,		40,		383,	352],
